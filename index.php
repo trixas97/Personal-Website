714c4398c35c226a10828cdb64cd47f7f5edab5f
trixas97 <33316040+trixas97@users.noreply.github.com>
2020-08-16 19:01:43 +0300

    Add files via upload
    First fiiles

---
+++ index.php
@@ -0,0 +1,40 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="menu">
+        <div class="menu-header"></div>
+        <div class="menu-body">
+            <ul class="menu-ul">
+                <li>Προσωπικές Πληροφορίες</li>
+                <li>Εργασιακή Εμπειρία</li>
+                <li>Τα έργα μου</li>
+                <li>Χρονοδιάγραμμα</li>
+                <li>Στοιχεία Επικοινωνίας</li>
+            </ul>
+        </div>
+    </div>    
+    <div class="data">
+        <h1><p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+        <p>asdkjakdkasbkbk</p>
+
+        </h1>
+    </div>
+</body>
+</html>
