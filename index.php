3caffa6a7719a4bdb99f555c180ff0768022b596
trixas <trixasmixas@gmail.com>
2020-09-01 19:00:09 +0300

    update Φορμα Επικοινωνίας

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
             <ul class="menu-ul"><h3>
                 <li class="menu-ul-li-checked"><a href="#">Προσωπικές Πληροφορίες</a></li>
                 <li><a href="#">Εργασιακή Εμπειρία</a></li>
-                <li><a href="#">Τα έργα μου</a></li>
+                <li><a href="#">Τα πρότζεκτ μου</a></li>
                 <li><a href="#">Χρονοδιάγραμμα</a></li>
-                <li><a href="#">Στοιχεία Επικοινωνίας</a></li></h3>
+                <li><a href="#">Φόρμα Επικοινωνίας</a></li></h3>
             </ul>
             <a href="https://www.facebook.com/trixas97" target="_blank"><img class="menu-icons" src="facebook.png"></a>
             <a href="https://github.com/trixas97" target="_blank"><img class="menu-icons" src="github.png"><a>
@@ -27,7 +27,18 @@
     <div class="data">
 
         <div class="data-first">
-            <h1 class="data-first-title">Στοιχεία Επικοινωνίας</h1>
+            <h1 class="data-first-title">Φόρμα Επικοινωνίας</h1>
+            <form style="margin-top: 5%;">
+            <label class="label-form">Επίθετο:</label><br>
+            <input class="text-form" type="text">
+            <label class="label-form">Όνομα:</label><br>
+            <input class="text-form" type="text">
+            <label class="label-form">Email:</label><br>
+            <input class="text-form" type="text">
+            <label class="label-form">Μήνυμα:</label><br>
+            <textarea class="text-form" name="" id="" cols="30" rows="10"></textarea>
+            
+
                     <!-- <h1><p>asdkjakdkasbkbk</p>
         <p>asdkjakdkasbkbk</p>
         <p>asdkjakdkasbkbk</p>
@@ -44,6 +55,16 @@
         <p>asdkjakdkasbkbk</p> -->
 
         <!-- </h1> -->
+
+        <div id="container">
+            <button class="learn-more" name="newwords">
+                <span class="circle" aria-hidden="true">
+                    <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Υποβολη</span>
+            </button>
+         </div>
+            </form>
 
         </div>
 
